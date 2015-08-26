fe0d7438aa2b94d90ce12f315440615c87fb0344
Vasco <vascko@sapo.pt>
2015-08-26 19:58:47 +0100

    Removed  HTTP's  Header 'accept' requirement to be of type json

--- Readme.md
+++ Readme.md
@@ -3,6 +3,15 @@
 To fill the need of having 2 repositories in sync, namely JIRA and MantisBT. 
 
 This is a Plugin for Mantis(1.2.19).
+___
+##### Install
+> Download or clone the repository to ```\mantis\plugins```
+
+>Login in ```mantis```, and Install the plugin in ``` Manage >  Manage Plugins``` 
+
+##### Use
+Webhooks should be sent to:
+> mantis.example.com```/plugin.php?page=JiraMantisInSync/connect```
 
 
 ___
--- pages/http/request.class.php
+++ pages/http/request.class.php
@@ -5,19 +5,14 @@ class Request
 	/**
 	 *	An HTTP request.
 	 */
-	public function populate($url, $method, $username, $password, $body=NULL, $type_expected='text/x-json')
+	public function populate($url, $method, $username, $password, $body=NULL)
 	{
 		$this->url = $url;
 		$this->method = $method;
 		$this->username = $username;
 		$this->password = $password;
-		$this->type_expected = $type_expected;
 		$this->body = $body;
 
-		//$parsed_url = parse_url($this->url);
-		//$this->host = $parsed_url['host'];
-		//$this->path = $parsed_url['path'];
-		//$this->query = $parsed_url['query'];
 	}
 
 	public function populate_from_server()
@@ -25,25 +20,6 @@ class Request
 		/**
 		 * 	Sets the Request's variables based on the incoming HTTP request.
 		 */
-		$this->url = $_SERVER['REQUEST_URI'];
-		$this->method = $_SERVER['REQUEST_METHOD'];
-		$this->username = $_SERVER['PHP_AUTH_USER'];
-		$this->password = $_SERVER['PHP_AUTH_PW'];
-		$this->body = file_get_contents('php://input');
-
-		//$this->_extrapolate_from_url();
-
-		$headers = getallheaders();
-		$type = array_key_exists('Accept', $headers)
-						? $headers['Accept']
-						: 'text/x-json';
-		if ($type == 'text/x-json' || $type == 'application/json') {
-			$this->type_expected = $type;
-		} else {
-			throw new HTTPException(406, "Unacceptable content type: $type.  This resource is available in the following content types:
-
-text/x-json
-application/json");
-		}
+    $this->populate($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD'], $_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW'], file_get_contents('php://input'));
 	}
 }
